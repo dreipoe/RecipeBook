021ba7ac4042be42560f29c684f46ea3edfc60e0
dreipoe <oreshkin1996@ya.ru>
2021-11-03 15:07:18 +0500

    1. Обновлены фикстуры 2. Обновлена вёрстка 3. Добавлена пасхалка xD

--- app/main/urls.py
+++ app/main/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = (
     path('', views.index, name='index'),
+    path('receipt/<int:idx>', views.detail, name='detail')
 )
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Receipt
 
@@ -8,5 +8,5 @@ def index(request: WSGIRequest):
     return render(request, "main.html", {'title': "Книга рецептов", 'items': Receipt.objects.all()})
 
 
-def view(request: WSGIRequest, idx: int):
-    pass
+def detail(request: WSGIRequest, idx: int):
+    return render(request, "detail.html", {"item": get_object_or_404(Receipt, pk=idx)})
